6115a40dddcf8ae1ec98c1c130dc165d5fa84b80
egileekada <egileoniso.ekada@gmail.com>
2026-02-26 03:27:29 +0100

    Remove unused utility files for date formatting, date status, number formatting, and socket.io integration to streamline the codebase.

--- helper/utils/numberFormat.ts
+++
@@ -1,9 +0,0 @@
-import formatter from "format-number";
-
-export const formatNumber = (number: number, prefix = "$") => {
-  // if(number === "***") {
-  //   return prefix+" ****"
-  // } else {
-  return formatter({ prefix })(number);
-  // }
-};
